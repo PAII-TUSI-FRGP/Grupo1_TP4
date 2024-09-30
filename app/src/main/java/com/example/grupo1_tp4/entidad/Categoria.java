d7c69020cf9994ea57c1c1d4805b1b20215ccff1
Joselo Esteban Orellano <mobilelegendsbang2020bang@gmail.com>
2024-09-30 01:01:04 -0300

    tk 01 realizada con exito

---
+++ app/src/main/java/com/example/grupo1_tp4/entidad/Categoria.java
@@ -0,0 +1,38 @@
+package com.example.grupo1_tp4.entidad;
+
+public class Categoria {
+    private Integer id;
+    private String descripcion;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
